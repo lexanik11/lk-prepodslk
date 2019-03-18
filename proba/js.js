8eafabbd93c3fc6ea277e0b1ba0cb794b32385ef
lexanik11 <47949620+lexanik11@users.noreply.github.com>
2019-03-18 03:06:37 +0300

    Add files via upload

---
+++ proba/js.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+	$('.one').on('click', function () {
+		$('.one-list').toggleClass('selected');
+	});
+
+	$('.two').on('click', function () {
+		$('.two-list').toggleClass('selected');
+	});
+
+	$('.three').on('click', function () {
+		$('.three-list').toggleClass('selected');
+	});
+
+});
